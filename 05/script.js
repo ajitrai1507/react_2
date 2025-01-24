09dac819de54bba8b1939c5333b1cff068b1b21f
ajitrai1507 <ajitrai0715@gmail.com>
2025-01-24 15:04:03 +0530

    Basic of react

---
+++ 05/script.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM, { createRoot } from 'react-dom/client';
+
+const element1 = <h1>Hello coder army</h1>
+
+const div = ReactDOM.createRoot(document.getElementById('root'));
+
+div.render(element1);
